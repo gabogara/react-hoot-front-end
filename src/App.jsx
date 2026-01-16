732c620f8e577ea472929f77019adc1002594119
gabogara <gabo1gara@gmail.com>
2026-01-15 17:42:19 -0800

    feat: add JWT-authenticated user fetch and render users on dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import NavBar from "./components/NavBar/NavBar";
+import { useContext } from "react";
+import { Routes, Route } from "react-router";
 
+import NavBar from "./components/NavBar/NavBar";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from "./components/SignInForm/SignInForm";
+// Import the Landing and Dashboard components
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+import { UserContext } from "./contexts/UserContext";
 const App = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+      <Routes>
+        {/* Add the new `/` route! */}
+        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/sign-in" element={<SignInForm />} />
+      </Routes>
     </>
   );
 };
